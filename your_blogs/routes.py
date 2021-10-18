22d3cd82bc6c2d75ee7209d2b6cb6c5dc46ee319
shon-arch <sunnywahane14@gmail.com>
2021-10-19 00:31:26 +0530

    Modified the login and register view function to actually log the user and added logout view function as well

--- your_blogs/routes.py
+++ your_blogs/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, url_for, flash, redirect
-from your_blogs import app
+from flask import render_template, url_for, flash, redirect, request
+from your_blogs import app, bcrypt, db
 from your_blogs.models import User, Post
 from your_blogs.forms import RegisterForm, LoginForm
-
+from flask_login import login_user, current_user, logout_user, login_required
 
 # To Do ;
 # replace dummy data with actual data using a database
@@ -35,17 +35,42 @@ def about():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegisterForm()
     if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username = form.username.data, password_hash= hashed_password, email = form.email.data)
+        db.session.add(user)
+        db.session.commit()
         flash(f"Account created for {form.username.data}", 'success')
-        return redirect(url_for('home'))
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash("Login Successful", 'success')
-        return redirect(url_for('home'))
+        user = User.query.filter_by(username = form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password_hash, form.password.data):
+            login_user(user, remember=form.remember.data)
+            flash("Login Successful", 'success')
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsucessful. Please check your username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title= 'Account')
