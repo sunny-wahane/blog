729169595fc0ce7d08ec43d04d7a3403e8c63eec
shon-arch <sunnywahane14@gmail.com>
2021-10-19 00:31:52 +0530

    Added custom validators

--- your_blogs/forms.py
+++ your_blogs/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, EqualTo, Email
+from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
+from your_blogs.models import User
 
 
 class RegisterForm(FlaskForm):
@@ -9,6 +10,16 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError('Username already taken')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email = email.data).first()
+        if user:
+            raise ValidationError('Email already taken')
 
 
 class LoginForm(FlaskForm):
