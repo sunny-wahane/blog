08cbf377c5f2c8370c5ab693087aa2572bc5448a
shon-arch <sunnywahane14@gmail.com>
2021-11-06 11:42:51 +0530

    change to the right file extenstion

--- your_blogs/models.py
+++ your_blogs/models.py
@@ -10,7 +10,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    avatar = db.Column(db.String(20), nullable=False, default='default.jpg')
+    avatar = db.Column(db.String(20), nullable=False, default='default.png')
     password_hash = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
