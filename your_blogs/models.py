109f8eadb5ee25315ec877002fec123763e7da36
shon-arch <sunnywahane14@gmail.com>
2021-12-19 23:07:54 +0530

    Added methods in models.py for generating tokens when the user wants to reset password

--- your_blogs/models.py
+++ your_blogs/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from your_blogs import db, login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from your_blogs import db, login_manager, app
 from flask_login import UserMixin
 
 @login_manager.user_loader
@@ -14,6 +15,20 @@ class User(db.Model, UserMixin):
     password_hash = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
+    
     def __repr__(self):
         return self.username
 
@@ -21,7 +36,7 @@ class User(db.Model, UserMixin):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    data_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     body = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
