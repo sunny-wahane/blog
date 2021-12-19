2a13eebad785fbb6304db91eae22fa198146e1e4
shon-arch <sunnywahane14@gmail.com>
2021-12-19 23:10:41 +0530

    registering all the blueprints to the app and intialsing the Mail object for sending mails to users for password reset

--- your_blogs/__init__.py
+++ your_blogs/__init__.py
@@ -2,19 +2,27 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from your_blogs.config import Config
 
 app = Flask(__name__)
 
-# To Do :
-# replace the secret key
-app.config['SECRET_KEY'] = '9c47844038dd8fe3543a9c0b080e8572'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
-from your_blogs import routes
+mail = Mail(app)
+
+from your_blogs.users.routes import users
+from your_blogs.posts.routes import posts
+from your_blogs.main.routes import main
+from your_blogs.errors.handler import errors
+
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
+app.register_blueprint(errors)
